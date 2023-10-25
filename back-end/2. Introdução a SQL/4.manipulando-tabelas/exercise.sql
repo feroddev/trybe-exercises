f87debeb0beb45f291aef2a74e66d99bfdd19083
Felipe Rodrigues <neath.felipe@gmail.com>
2023-10-25 14:06:58 -0300

    Finaliza os exercicios do dia

---
+++ back-end/2. Introdução a SQL/4.manipulando-tabelas/exercise.sql
@@ -0,0 +1,39 @@
+USE pixar;
+-- Exercício 1: 
+INSERT INTO movies(title, director, `year`, length_minutes)
+    VALUES ('Monstros SA', 'Pete Docter', 2001, 92),
+    ('Procurando Nemo', 'John Lasseter', 2003, 107),
+    ('Os Incríveis', 'Brad Bird', 2004, 116),
+    ('WALL-E', 'Pete Docter', 2008, 104);
+
+-- Exercício 2: 
+INSERT INTO box_office(movie_id, rating, domestic_sales, international_sales)
+    VALUES(9, 6.8, 450000000, 370000000);
+
+-- Exercício 3:
+UPDATE movies
+    SET director = 'Andrew Stanton'
+    WHERE title = 'Procurando Nemo';
+
+-- Exercício 4:
+UPDATE movies
+    SET title = 'Ratatouille', `year` = 2007
+    WHERE id = 3;
+
+-- Exercício 5: 
+INSERT INTO box_office(movie_id, rating, domestic_sales, international_sales)
+    VALUES(8, 8.5, 300000000, 250000000),
+    (10, 7.4, 460000000, 510000000),
+    (11, 9.9, 290000000, 280000000);
+
+-- Exercício 6: 
+DELETE FROM box_office
+    WHERE movie_id = 11;
+DELETE FROM movies
+    WHERE title = 'WALL-E';
+
+-- Exercício 7:
+DELETE FROM box_office
+    WHERE movie_id IN(2,9);
+DELETE FROM movies
+    WHERE director = 'Andrew Stanton';
