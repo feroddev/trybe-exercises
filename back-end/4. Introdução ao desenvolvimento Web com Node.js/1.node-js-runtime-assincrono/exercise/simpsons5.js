1b44f84cdd8bc14e18c3e32587fd1e5c2a8d3d88
Felipe Rodrigues <neath.felipe@gmail.com>
2023-11-06 01:56:43 -0300

    Finaliza 6 desafio

--- back-end/4. Introdução ao desenvolvimento Web com Node.js/1.node-js-runtime-assincrono/exercise/simpsons5.js
+++ back-end/4. Introdução ao desenvolvimento Web com Node.js/1.node-js-runtime-assincrono/exercise/simpsons5.js
@@ -1,7 +1,7 @@
 const fs = require("fs").promises;
 
 async function addNewSimpson() {
-  const fileContent = await fs.readFile(".simpsonsFamily.json", 'utf-8');
+  const fileContent = await fs.readFile("./simpsonsFamily.json", 'utf-8');
   const simpsonsFamily = JSON.parse(fileContent);
   simpsonsFamily.push({ id: '8', name: 'Nelson Muntz' });
   await fs.writeFile('./simpsonsFamily.json', JSON.stringify(simpsonsFamily));
