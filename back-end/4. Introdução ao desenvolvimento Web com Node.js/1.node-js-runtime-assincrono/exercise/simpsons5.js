fbdf6e7ed7a893936de6cfce9a0ca13a3d0ce9f3
Felipe Rodrigues <neath.felipe@gmail.com>
2023-11-06 01:48:53 -0300

    Finaliza 5 desafio

---
+++ back-end/4. Introdução ao desenvolvimento Web com Node.js/1.node-js-runtime-assincrono/exercise/simpsons5.js
@@ -0,0 +1,14 @@
+const fs = require("fs").promises;
+
+async function addNewSimpson() {
+  const fileContent = await fs.readFile(".simpsonsFamily.json", 'utf-8');
+  const simpsonsFamily = JSON.parse(fileContent);
+  simpsonsFamily.push({ id: '8', name: 'Nelson Muntz' });
+  await fs.writeFile('./simpsonsFamily.json', JSON.stringify(simpsonsFamily));
+}
+
+function main(){
+  addNewSimpson();
+}
+
+main();
