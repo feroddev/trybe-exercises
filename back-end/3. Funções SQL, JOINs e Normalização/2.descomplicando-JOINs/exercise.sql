c389084709733fffc4ccdc044c4ab03dbf5c6790
Felipe Rodrigues <neath.felipe@gmail.com>
2023-11-07 23:20:14 -0300

    Finaliza exercicios do dia 2

---
+++ back-end/3. Funções SQL, JOINs e Normalização/2.descomplicando-JOINs/exercise.sql
@@ -0,0 +1,76 @@
+USE pixar;
+
+-- 1 
+SELECT
+    m.title, b.domestic_sales, b.international_sales
+FROM
+    movies m
+        INNER JOIN
+    box_office b ON b.movie_id = m.id;
+
+-- 2
+SELECT
+    m.title,
+    (b.domestic_sales + b.international_sales) AS "Vendas"
+FROM
+    movies m
+        INNER JOIN
+    box_office b ON b.movie_id = m.id
+WHERE
+    b.international_sales > b.domestic_sales;
+
+-- 3
+SELECT
+    m.title, b.rating
+FROM
+    movies m
+INNER JOIN
+    box_office b ON b.movie_id = m.id
+ORDER BY b.rating DESC;
+
+-- 4
+SELECT
+    t.name,
+    t.location,
+    m.title,
+    m.director,
+    m.year,
+    m.length_minutes
+FROM
+    theater t
+LEFT JOIN
+    movies m ON t.id = m.theater_id
+ORDER BY t.name;
+
+-- 5
+USE pixar;
+
+SELECT
+    t.id,
+    t.name,
+    t.location,
+    m.title,
+    m.director,
+    m.year,
+    m.length_minutes
+FROM
+    theater t
+RIGHT JOIN
+    movies m ON t.id = m.theater_id
+ORDER BY t.name;
+
+-- BONUS 6 
+SELECT
+    m.id,
+    m.title,
+    m.director,
+    m.year,
+    m.length_minutes,
+    m.theater_id
+FROM
+    movies m
+INNER JOIN
+    box_office b ON b.movie_id = m.id
+WHERE
+    b.rating > 8
+        AND m.theater_id IS NOT NULL;
